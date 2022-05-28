c458c8b61cad53b555fe8617cc54cd4fce6d258b
Omprakash Choudhary <om78900987@gmail.com>
2022-05-28 22:08:19 +0530

    Time: 19 ms (84.65%), Space: 17.9 MB (65.37%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& a) {
-        int mn=*min_element(a.begin(), a.end());
-        int mx=*max_element(a.begin(), a.end());
-        unordered_map<int,bool> map;
-        for(int i=0;i<a.size();i++)
-            map[a[i]]=1;
-        mn=mn!=0?mn-1:mn;
-        int no=-1;
-        for(int i=mn;i<=mx;i++){ 
-          
-            if(map.count(i)<=0){
-                no=i;
-                break;
-            }
-        }
-        if(no==-1)no=mx+1;
-        return no;
+        int sum=accumulate(a.begin(), a.end(), 0);
+        int n=a.size();
+        int sumofn=((n+1)*n)/2;
+        return sumofn-sum;
     }
 };
