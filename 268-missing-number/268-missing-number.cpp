36f13a56c12053a0f987d214fd1fe17612a2cc4c
Omprakash Choudhary <om78900987@gmail.com>
2022-05-28 21:48:49 +0530

    Time: 87 ms (5.13%), Space: 24.3 MB (5.21%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -4,17 +4,16 @@ public:
         int mn=*min_element(a.begin(), a.end());
         int mx=*max_element(a.begin(), a.end());
         unordered_map<int,bool> map;
-        for(int i=0;i<a.size();i++){
+        for(int i=0;i<a.size();i++)
             map[a[i]]=1;
-            
-        }
+        mn=mn!=0?mn-1:mn;
         int no=-1;
-        if(mn!=0)
-            mn=0;
         for(int i=mn;i<=mx;i++){ 
           
-            if(map.count(i)<=0)
+            if(map.count(i)<=0){
                 no=i;
+                break;
+            }
         }
         if(no==-1)no=mx+1;
         return no;
