d7827547c5571b6b317d21988f0d3107d09633e6
Omprakash Choudhary <om78900987@gmail.com>
2022-07-14 12:12:12 +0530

    Time: 22 ms (40.49%), Space: 10.6 MB (49.53%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        vector<vector<char>> v(numRows,vector<char>(s.size(),'0'));
-        string str="";
-        int k=0,col=0;
-        while(k<s.size()){
-            int i=0;
-            for(i=0;i<numRows and k<s.size();i++){
-                v[i][col]=s[k++];
-            }
-            col++;i-=2;
-            int j=col;
-            if(k==s.size())break;
-            for(;i>=1 and j<(col+numRows-1) and k<s.size();i--,j++){
-                v[i][j]=s[k++];
-            }
-            col=j;
+        if(numRows<=1)return s;
+       string temp="";
+        vector<string> dp(numRows,"");
+        for(int i=0,row=0,step=1;i<s.size();i++){
+            dp[row]+=s[i];
+            if(row==0) step=1;
+            else if(row==numRows-1) step=-1;
+            row+=step;
         }
-        for(int i=0;i<v.size();i++){
-            for(int j=0;j<v[i].size();j++){
-                if(v[i][j]!='0')
-                    str+=v[i][j];
-            }
-        }
-        return str;
+        for(auto it:dp)
+            temp+=it;
+        return temp;
     }
 };
