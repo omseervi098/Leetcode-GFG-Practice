e19c96e0edd437372e372221cc13cdc852d7c26d
Omprakash Choudhary <om78900987@gmail.com>
2022-07-16 13:54:39 +0530

    Time: 115 ms (5.03%), Space: 27.5 MB (5.02%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int idxof0=-1;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>=0){idxof0=i;break;}
+        }cout<<idxof0<<endl;
+        if(idxof0==-1){
+            vector<int>num(nums.begin(),nums.end());
+            for(auto &it:num)it*=it;
+            reverse(num.begin(),num.end());return num;
+        }
+        vector<int> ans(nums.begin(),nums.begin()+idxof0);
+        reverse(ans.begin(),ans.end());
+        vector<int>num(nums.begin()+idxof0,nums.end());
+        for(auto &it:ans)
+            it=it*it;
+        for(auto &it:num)
+            it=it*it;
+        for(auto it:ans){
+            cout<<it<<" "; 
+        }for(auto it:num){
+            cout<<it<<" ";
+        }cout<<endl;
+        vector<int> fans;
+        int i=0,j=0;
+        int m=ans.size(),n=num.size();
+        while(i<m and j<n){
+            if(ans[i]<num[j]){
+                fans.push_back(ans[i]);
+                i++;
+            }else {
+                fans.push_back(num[j]);
+                j++;
+            }
+        }
+        while(i<m){
+            fans.push_back(ans[i]);
+            i++;
+        }
+        while(j<n){
+            fans.push_back(num[j]);
+            j++;
+        }
+    
+        return fans;
+    }
+};
