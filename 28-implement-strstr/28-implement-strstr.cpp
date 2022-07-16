5f95527bbe655d266ed045a21522345e54014fcc
Omprakash Choudhary <om78900987@gmail.com>
2022-07-16 14:35:42 +0530

    Time: 8 ms (5.12%), Space: 6.4 MB (25.34%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.cpp
+++ 28-implement-strstr/28-implement-strstr.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
+        //o(m*n)
+        //we can use kmp algo to optimize to o(m)
         if(haystack.size()<needle.size())return -1;
         for(int i=0;i<haystack.size()-needle.size()+1;i++){
             string temp=haystack.substr(i,needle.size());
