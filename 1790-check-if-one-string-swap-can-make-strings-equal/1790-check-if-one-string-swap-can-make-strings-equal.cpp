29c69bfccdde3a31fa94170e2d9a694d5ca29a80
Omprakash Choudhary <om78900987@gmail.com>
2022-07-24 12:02:45 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (85.19%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -3,13 +3,17 @@ public:
     bool areAlmostEqual(string s1, string s2) {
         int n=s1.size(),m=s2.size();
         if(s1==s2)return true;
+        int swapfirst=-1;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                swap(s1[i],s1[j]);
-                if(s1==s2)return true;
-                else
-                    swap(s1[i],s1[j]);
+            if(s1[i]!=s2[i]){
+                if(swapfirst==-1){
+                    swapfirst=i;
+                }
+                else{
+                    swap(s1[i],s1[swapfirst]);
+                    return s1==s2;
+                }
             }
-        }return false;
+        }return s1==s2;
     }
 };
