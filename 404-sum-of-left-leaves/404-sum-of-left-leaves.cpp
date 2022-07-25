2dcaebf3e451b200092beef1fcf9b584d8fac6d5
Omprakash Choudhary <om78900987@gmail.com>
2022-07-25 08:20:26 +0530

    Time: 3 ms (86.63%), Space: 13.3 MB (81.38%) - LeetHub

--- 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/404-sum-of-left-leaves.cpp
@@ -12,16 +12,13 @@
 class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        if(root==NULL)
-            return 0;
+        if(root==nullptr||(root->left==nullptr and root->right==nullptr))return 0;
         int left=0,right=0;
         if(root->left and root->left->left==NULL and root->left->right==NULL)
             left=root->left->val;
         else
-        left=sumOfLeftLeaves(root->left);
-        //if(root->right and root->right->left!=NULL and root->right->right!=NULL)
-        right=sumOfLeftLeaves(root->right);
+            left=sumOfLeftLeaves(root->left);
+            right=sumOfLeftLeaves(root->right);
         return left+right;
-        
     }
 };
