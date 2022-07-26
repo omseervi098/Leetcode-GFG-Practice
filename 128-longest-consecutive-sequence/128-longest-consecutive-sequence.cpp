aac3e3e1a3cc122d4aa913e7324ff36212f6f3cb
Omprakash Choudhary <om78900987@gmail.com>
2022-07-26 22:21:12 +0530

    Time: 78 ms (97.39%), Space: 33.8 MB (81.54%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -4,21 +4,17 @@ public:
         int n=arr.size();
         sort(arr.begin(),arr.end());
         int mxlen=0;
-       for(int i=0;i<n;i++){
-        int st=arr[i];
-        int k=i;
-          int temp=0;
-        while(i<n-1 and (arr[i]+1==arr[i+1]||arr[i]==arr[i+1])){
-            if(arr[i]==arr[i+1]) temp++;
-            i++;
+        for(int i=0;i<n;i++){
+           int st=arr[i];
+           int k=i;
+           int skips=0;
+           while(i<n-1 and (arr[i]+1==arr[i+1]||arr[i]==arr[i+1])){
+              if(arr[i]==arr[i+1]) skips++;
+              i++;
+           }
+           if((i-k-skips)+1>=mxlen)
+              mxlen=(i-k-skips)+1;   
         }
-        cout<<temp<<endl;
-        if((i-k-temp)+1>=mxlen){
-            mxlen=(i-k-temp)+1;
-            cout<<mxlen<<endl;
-        }   
-        // 0 1 1 2
-    }
-    return mxlen;
+        return mxlen;
     }
 };
