1e406ffbdcd2e2c2c6a45f6ba1ab2282f1500763
Omprakash Choudhary <om78900987@gmail.com>
2022-08-03 09:38:17 +0530

    Time: 16 ms (5.28%), Space: 7.2 MB (16.92%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    string s;
+    unordered_map<string,string> s;
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        s=longUrl;
-        return s;
+        string alpha="qwertyuioplkjhgfdsazxcvbnm1234567890";
+        string base="https::/tinyurl.com/",ans="";
+        srand(time(0));
+        for(int i=0;i<6;i++){
+            ans+=alpha[rand()%36];
+        }
+        s[ans+base]=longUrl;
+        return ans+base;
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-       return s; 
+       return s[shortUrl]; 
     }
 };
 
