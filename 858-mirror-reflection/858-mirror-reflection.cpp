60977c52014df32bfda509bfd0e93e8a1e004f9f
Omprakash Choudhary <om78900987@gmail.com>
2022-08-04 20:17:32 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (90.91%) - LeetHub

---
+++ 858-mirror-reflection/858-mirror-reflection.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int mirrorReflection(int p, int q) {
+        int lcm=(p*q)/(__gcd(p,q));
+        int m=lcm/p;
+        int n=lcm/q;
+        if(m%2==0 and n%2==1) return 0;
+        if(m%2==1 and n%2==1) return 1;
+        if(m%2==1 and n%2==0) return 2;
+        return -1;
+    }
+};
