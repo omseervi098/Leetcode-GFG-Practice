a0ac20bcf158d6fb7c06b213e257a53f1171bbaf
Omprakash Choudhary <om78900987@gmail.com>
2022-08-05 20:54:34 +0530

    Time: 7 ms (19.94%), Space: 6.7 MB (30.10%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -14,9 +14,16 @@ public:
         return inc;
     }
     int combinationSum4(vector<int>& nums, int target) {
-        int n=nums.size();
-        vector<int> v(target+1,-1);
-        
-        return helper(nums,target,0,v);
+        vector<unsigned int> dp(target+1,0);
+        dp[0]=1;
+        for(int i=1;i<=target;i++){
+            
+            for(int j=0;j<nums.size();j++){
+                if(i>=nums[j]){
+                    dp[i]=dp[i]+dp[i-nums[j]];
+                }
+            }
+        }
+        return dp[target];
     }
 };
