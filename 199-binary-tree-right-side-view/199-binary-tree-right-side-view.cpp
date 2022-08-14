56463c37fbc836ef7e04c824db3dea3d369aa89d
Omprakash Choudhary <om78900987@gmail.com>
2022-08-14 22:01:27 +0530

    Time: 3 ms (85.34%), Space: 12 MB (44.33%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -8,27 +8,50 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+//  */
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root) {
+//         if(root==nullptr) return {};
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         vector<int> ans;
+//         while(!q.empty()){
+//             int n=q.size();
+//             for(int i=0;i<n;i++){
+//                 TreeNode* front=q.front();
+//             q.pop();
+//                 if(i==n-1)
+//                     ans.push_back(front->val);
+//             if(front->left)
+//                 q.push(front->left);
+//             if(front->right)
+//                 q.push(front->right);
+//             }
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-        if(root==nullptr) return {};
-        queue<TreeNode*>q;
-        q.push(root);
-        vector<int> ans;
-        while(!q.empty()){
-            int n=q.size();
-            for(int i=0;i<n;i++){
-                TreeNode* front=q.front();
-            q.pop();
-                if(i==n-1)
-                    ans.push_back(front->val);
-            if(front->left)
-                q.push(front->left);
-            if(front->right)
-                q.push(front->right);
-            }
+        vector<int> view;
+        int idx=0;
+        rightView(root, view, 0,idx);
+        return view;
+    }
+private:
+    void rightView(TreeNode* root, vector<int>& view, int level,int &idx) {
+        if (!root) {
+            return;
         }
-        return ans;
+        cout<<idx<<endl;
+        if (idx == level) {
+            view.push_back(root -> val);
+            idx++;
+        }
+        
+        rightView(root -> right, view, level + 1,idx);
+        rightView(root -> left, view, level + 1,idx);
     }
 };
