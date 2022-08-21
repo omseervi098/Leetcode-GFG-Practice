9bc1bf86f3e67fb5b0b932da526827044243af30
Omprakash Choudhary <om78900987@gmail.com>
2022-08-21 21:53:44 +0530

    Time: 18 ms (76.47%), Space: 13.2 MB (82.05%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,25 +3,33 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        unordered_set<int> s;
-        unordered_set<int> s1;
+        bool r=false,c=false;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    s.insert(i);
-                    s1.insert(j);   
+                    if(i==0) r=true;
+                    if(j==0) c=true;
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                   
                 }
             }
-        } 
-        for(auto it:s){
-            cout<<it<<endl;
-            for(int i=0;i<n;i++){
-                matrix[it][i]=0;
-            }
         }
-        for(auto it:s1){
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0||matrix[0][j]==0)
+                  matrix[i][j]=0;
+                
+            }
+        } 
+       
+        if(c){
             for(int i=0;i<m;i++){
-                matrix[i][it]=0;
+                matrix[i][0]=0;
+            }
+        } if(r){
+            for(int j=0;j<n;j++){
+                matrix[0][j]=0;
             }
         }
         return;
