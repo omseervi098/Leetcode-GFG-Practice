46f4f5df5baa0dd0dbb4762c81ae5dedf2b925cd
Omprakash Choudhary <om78900987@gmail.com>
2022-08-24 23:17:55 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (91.93%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -2,30 +2,17 @@ class Solution {
 public:
     bool convertToTernary(int N)
    {
-    // Base case
-    if (N == 0)
-        return true;
- 
-    // Finding the remainder
-    // when N is divided by 3
-    int x = N % 3;
-    N /= 3;
-    if (x < 0)
-        N += 1;
-    if(x==2) return false;
- 
-    // Recursive function to
-    // call the function for
-    // the integer division
-    // of the value N/3
-    bool ans=convertToTernary(N);
- 
-    // Handling the negative cases
-    
+        if (N == 0)
+           return true;
+        int x = N % 3;
+        N /= 3;
+        if (x < 0)
+           N += 1;
+        if(x==2) return false;
+        bool ans=convertToTernary(N);
         return ans;
    }
     bool checkPowersOfThree(int n) {
-        
         return convertToTernary(n);
     }
 };
