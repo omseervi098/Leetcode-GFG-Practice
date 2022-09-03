c28075fe830c30552bf6cf83475486c36661e849
Omprakash Choudhary <om78900987@gmail.com>
2022-09-03 20:48:20 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (29.51%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int divide(int div, int d) {
+        int neg=1;
+        
+        if((div>0 and d>0) || (div<0 and d<0)){
+            if(d==-1 and div<=INT_MIN) return INT_MAX;
+            neg=1;
+        }else{
+            
+            neg=-1;
+        }
+        //cout<<neg<<endl;
+       long long int a=abs(div);
+       long long int b=abs(d);
+        cout<<a<<" "<<b<<endl;
+        unsigned int curr=1;
+        int ans=0;
+        if(b==1) return neg*a;
+        if(b==0){
+            return -1;
+        }
+        if(a<b){
+            return 0;
+        }
+        if(a==b){
+            return neg*1;
+        }
+        
+        while(a>=b){
+            b<<=1;
+            curr<<=1;
+        }
+        b>>=1;
+        curr>>=1;
+        while(curr!=0){
+            if(a>=b){
+                a-=b;
+                ans=ans|curr;
+            }
+            b>>=1;
+            curr>>=1;
+        }
+       // cout<<ans<<endl;
+        return ans*neg;
+    }
+};
