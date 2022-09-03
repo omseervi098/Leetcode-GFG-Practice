8fc42819861060f4b1118b7cf6a583a99441d70a
Omprakash Choudhary <om78900987@gmail.com>
2022-09-03 19:28:14 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (66.72%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -6,26 +6,20 @@ public:
             v.push_back(num);
             return;
         }
-        //cout<<num<<endl;
         if(st+k<=9){
             int temp=num*10+st+k;
-           
             dfs(n-1,k,st+k,temp,v);
         }
-       // cout<<num<<endl;
         if(st+k!=st-k and st-k>=0){
             int temp=num*10+st-k;
-            
             dfs(n-1,k,st-k,temp,v);
-        }
-        
+        }     
     }
     vector<int> numsSameConsecDiff(int n, int k) {
         vector<int> v;
         for(int i=1;i<=9;i++){
-            int num=i;
-            dfs(n,k,i,num,v);
-            //cout<<s<<endl; 
+            
+            dfs(n,k,i,i,v);
         }
         return v;
     }
