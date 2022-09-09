9beea5d2622f5971f1097ab1fb5f29c201b8c036
Omprakash Choudhary <om78900987@gmail.com>
2022-09-09 22:33:16 +0530

    Time: 4 ms (91.79%), Space: 5.9 MB (78.94%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -4,35 +4,11 @@ public:
         return p1.second>p2.second;
     }
     string intToRoman(int num) {
-        vector<pair<string,int>> v;
-        v.push_back({"I",1});
-        v.push_back({"V",5});
-        v.push_back({"X",10});
-        v.push_back({"L",50});
-        v.push_back({"C",100});
-        v.push_back({"D",500});
-        v.push_back({"M",1000});
-        v.push_back({"CM",900});
-        v.push_back({"XC",90});
-        v.push_back({"CD",400});
-        v.push_back({"XL",40});
-        v.push_back({"IX",9});
-        v.push_back({"IV",4});
-        sort(v.begin(),v.end(),comp);
-
-        int temp=num;
-        string ans="";
-       
-            for(auto it:v){
-                while(temp>=it.second){
-                
-                    temp-=it.second;
-                    ans+=it.first;
-                    cout<<it.second<<" ";
-                
-                }
-            }
+        string ones[] = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        string tens[] = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        string hrns[] = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        string ths[]={"","M","MM","MMM"};
         
-        return ans;
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10];
     }
 };
