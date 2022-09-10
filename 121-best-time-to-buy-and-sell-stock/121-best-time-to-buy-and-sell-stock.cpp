d4a58ad61a507c771e9152c36381eb43477e5806
Omprakash Choudhary <om78900987@gmail.com>
2022-09-10 20:48:31 +0530

    Time: 894 ms (5.02%), Space: 163 MB (5.17%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        stack<int>s;int ans=0;
-        for(int i=0;i<prices.size();i++){
-            if(s.empty())s.push(prices[i]);
-            else if(s.top()>prices[i]) s.push(prices[i]);
-            ans=max(ans,prices[i]-s.top());
+    int find(vector<int> &prices,int st,int k,bool buy,vector<vector<int>> &v){
+        if(st>=prices.size() || k<=0){
+            return 0;
         }
-        return ans;
+        if(v[st][buy]!=-1)return v[st][buy];
+        if(buy){
+            int notbuy=find(prices,st+1,k,buy,v);
+            int bbuy=-prices[st]+find(prices,st+1,k,!buy,v);
+            return v[st][buy]=max(notbuy,bbuy);
+        }
+        else {
+            int notsell=find(prices,st+1,k,buy,v);
+            int sell=prices[st]+find(prices,st+1,k-1,!buy,v);
+            return v[st][buy]=max(notsell,sell);
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> v(n,vector<int>(2,-1));
+        return find(prices,0,1,1,v);
     }
 };
