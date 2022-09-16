12afc5c2d58aac89d65199816ddc778f5eaba7b2
Omprakash Choudhary <om78900987@gmail.com>
2022-09-16 20:24:12 +0530

    Time: 613 ms (64.89%), Space: 119.9 MB (71.96%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -8,12 +8,20 @@ public:
        
         if(dp[st][idx]!=INT_MIN) return dp[st][idx];
        
-        return dp[st][idx]=max((nums[st]*mul[idx]+helper(st+1,end,idx+1,nums,mul)),(nums[end]*mul[idx]+helper(st,end-1,idx+1,nums,mul)));
+        return dp[st][idx]=max((nums[st]*mul[idx]+helper(st+1,end,idx+1,nums,mul)),(nums[nums.size()-(idx-st)-1]*mul[idx]+helper(st,end,idx+1,nums,mul)));
         
     }
-    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
-        int m=multipliers.size();
-        dp.resize(m+1,vector<int>(m+1,INT_MIN));
-        return helper(0,nums.size()-1,0,nums,multipliers);
+    int maximumScore(vector<int>& nums, vector<int>& mul) {
+        int m=mul.size();
+        dp.resize(m+1,vector<int>(m+1,0));
+        for(int st=m-1;st>=0;st--){
+            for(int idx=m-1;idx>=st;idx--){
+                int e=nums.size()-(idx-st)-1;
+                int start=nums[st]*mul[idx]+dp[st+1][idx+1];
+                int end=nums[e]*mul[idx]+dp[st][idx+1];
+                dp[st][idx]=max(start,end);
+            }
+        }
+        return dp[0][0];
     }
 };
