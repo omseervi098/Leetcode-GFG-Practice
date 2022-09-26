d16f80f945fedb26520d3dde2ee96d7bf478870d
Omprakash Choudhary <om78900987@gmail.com>
2022-09-26 22:57:43 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (77.67%) - LeetHub

---
+++ 198-house-robber/198-house-robber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int helper(int * arr,int n,int* help){
+        if(n<=0){
+            return 0;
+        }
+        if(help[n]!=-1)
+            return help[n];
+        int x=arr[0]+helper(arr+2,n-2,help);
+        int y=helper(arr+1,n-1,help);
+        help[n]=max(x,y);
+        return help[n];
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        int arr[n];
+        for(int i=0;i<n;i++)
+            arr[i]=nums[i];
+        int help[n+1];
+        for(int i=0;i<=n;i++)
+            help[i]=-1;
+        return helper(arr,n,help);
+    }
+};
