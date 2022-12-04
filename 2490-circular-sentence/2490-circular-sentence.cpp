abd1dc2ca533a759116d2777de62482409530e24
Omprakash Choudhary <om78900987@gmail.com>
2022-12-04 17:21:59 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (60.00%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.cpp
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -3,12 +3,9 @@ public:
     bool isCircularSentence(string s) {
         int n=s.size();
         for(int i=0;i<n;i++){
-            if(s[i]==' '){
-                if(i+1<n and s[i+1]!=s[i-1]) return false;
-            }
-            if(i==n-1){
-                if(s[i]!=s[0]) return false;
-            }
-        }return true;
+            if(s[i]==' ' and i+1<n and s[i+1]!=s[i-1]) return false;
+            if(i==n-1 and s[i]!=s[0]) return false;
+        }
+        return true;
     }
 };
