0356d95016f42d4f7d375733a15828e433b2c0ae
Omprakash Choudhary <om78900987@gmail.com>
2022-12-04 13:48:00 +0530

    Time: 122 ms (95.74%), Space: 78.3 MB (96.38%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int minimumAverageDifference(vector<int>& nums) {
-        long long sm=0;
+        long long sm=0,pre=0,k=1,n=nums.size(),mn=INT_MAX,mnidx=0;
         for(auto i:nums) sm+=i;
-        long long pre=0,k=1,n=nums.size(),mn=INT_MAX,mnidx=0;
         for(int i=0;i<nums.size();i++){
             pre+=nums[i];
-            int check=0;
-            if(k<n) check=((sm-pre)/(n-k));
-            int diff=abs((pre/k++)-check);
+            int diff=pre/k;
+            if(k<n) diff=abs(pre/k-(sm-pre)/(n-k++));
             if(diff<mn){
                 mn=diff;
                 mnidx=i;
