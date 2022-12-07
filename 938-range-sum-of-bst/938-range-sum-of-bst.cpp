94d59f3f6787aa6e3139900b8377c2b0a3d7afae
Omprakash Choudhary <om78900987@gmail.com>
2022-12-08 00:13:28 +0530

    Time: 306 ms (37.49%), Space: 64.6 MB (60.79%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -19,10 +19,7 @@ public:
         if(root->val>=low and root->val<=high){
             sum+=root->val;
         }
-        if(root->val>low)
-            sum+=rangeSumBST(root->left,low,high);
-        if(root->val<high) 
-            sum+=rangeSumBST(root->right,low,high);
+        sum+=rangeSumBST(root->left,low,high)+rangeSumBST(root->right,low,high);
         return sum;
     }
 };
