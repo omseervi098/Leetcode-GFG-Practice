4c3ba06889c420a84ed20141bf00d11a3ffff515
Omprakash Choudhary <om78900987@gmail.com>
2023-04-03 20:46:01 +0530

    Time: 96 ms (25.80%), Space: 42.1 MB (11.29%) - LeetHub

--- 881-boats-to-save-people/881-boats-to-save-people.cpp
+++ 881-boats-to-save-people/881-boats-to-save-people.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
-    int binarysearch(vector<int> &people,int st,int limit){
-        int end=people.size()-1,mx=-1;
-        while(st<=end){
-            int mid=(st+end)/2;
-            if(people[mid]<=limit){
-                mx=mid;
-                st=mid+1;
-            }else{
-                end=mid-1;
-            }
-        }
-        return mx;
-    }
+    
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin(),people.end());
-        int cnt=0;
-        for(int i=0;i<people.size();i++){
-            if(people[i]!=INT_MAX){
-                int limitleft=limit-people[i];
-                if(limitleft==0)
-                    cnt++;
-                else if(limitleft>0){
-                    //Binary Search to get max element which is less than limitleft
-                    int val=binarysearch(people,i+1,limitleft);
-                    if(val!=-1){
-                        people[val]=INT_MAX; //change val to mark person visited
-                        cnt++;
-                    }else
-                        cnt++; //if we get no pair then take single person on boat
-                }
-            }
+        int end=people.size()-1,cnt=0,st=0;
+        while(st<=end){
+            if(people[st]+people[end]<=limit){
+                st++;end--;
+            }else
+                end--;
+            cnt++;
         }
         return cnt;
     }
