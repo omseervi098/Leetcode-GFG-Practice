88dba406f1088222681b19503ba9d4c8cf90b07b
Omprakash Choudhary <om78900987@gmail.com>
2023-04-19 15:41:10 +0530

    Time: 154 ms (95.82%), Space: 94.2 MB (63.41%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int helper(TreeNode* root,bool left,int d){
-        if(root==NULL) return d;
+        if(!root) return d;
         if(left) return max(helper(root->right,false,d+1),helper(root->left,true,0));
         return max(helper(root->left,true,d+1),helper(root->right,false,0));
     }
     int longestZigZag(TreeNode* root) {
-        int left=helper(root->left,true,0);
-        int right=helper(root->right,false,0);
-        return max(left,right);
+        return max(helper(root->left,true,0),helper(root->right,false,0));
     }
 };
