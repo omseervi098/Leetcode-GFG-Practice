e6866696db2f35827aeb67cf8a57e674514fa638
Omprakash Choudhary <om78900987@gmail.com>
2023-04-21 07:42:58 +0530

    Time: 384 ms (14.15%), Space: 93.9 MB (9.91%) - LeetHub

---
+++ 879-profitable-schemes/879-profitable-schemes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    vector<vector<vector<long long>>> dp;
+    const int mod=1e9+7;
+public:
+    long long helper(vector<int> &p,vector<int> &g,long long profit,int n,int st){
+        if(n<0){
+            return 0;
+        }
+        if(st>=p.size()){
+            // if(minprofit==0 and profit==0) return 1;
+            if(profit<=0) return 1;
+            return 0;
+        }
+        if(profit<0) profit=0;
+        if(dp[st][n][profit]!=-1) return dp[st][n][profit];
+        //excluding
+        int a=helper(p,g,profit,n,st+1)%mod;
+        //including
+       
+        int b=helper(p,g,profit-p[st],n-g[st],st+1)%mod;
+        return dp[st][n][profit]=(a+b)%mod;
+    }
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        dp.resize(profit.size()+1,vector<vector<long long>>(n+1,vector<long long>(minProfit+1,-1)));
+        return helper(profit,group,minProfit,n,0)%mod;
+    }
+};
