5988d9f5ef02e33e6ad75624490eee7dddf485a9
Omprakash Choudhary <om78900987@gmail.com>
2023-04-26 09:35:18 +0530

    Time: 7 ms (16.24%), Space: 6.1 MB (7.98%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,8 +1,12 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if(num==0)
-            return 0;
-        return num%9==0?9:num%9;
+        if(num>=0 and num<=9) return num;
+        int sm=0;
+        while(num){
+            sm+=num%10;
+            num/=10;
+        }
+        return addDigits(sm);
     }
 };
