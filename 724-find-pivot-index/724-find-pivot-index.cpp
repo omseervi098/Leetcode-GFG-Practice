2a829e032ac8097bb68da0fbd5969fd99b244599
Omprakash Choudhary <om78900987@gmail.com>
2023-05-05 09:37:48 +0530

    Time: 29 ms (43.13%), Space: 31.1 MB (93.20%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sm=accumulate(nums.begin(),nums.end(),0);
+        int tmp=0;
+        // if(sm==tmp)return 0;
+        for(int i=0;i<nums.size();i++){
+            sm-=nums[i];
+            if(sm==tmp) return i;
+            tmp+=nums[i];
+        }
+        return -1;
+    }
+};
