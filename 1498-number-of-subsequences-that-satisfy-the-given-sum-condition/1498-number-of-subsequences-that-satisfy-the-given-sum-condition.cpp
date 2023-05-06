f542946e185a45188d21fe62ba48900a32508d95
Omprakash Choudhary <om78900987@gmail.com>
2023-05-06 23:10:28 +0530

    Time: 238 ms (5.07%), Space: 48.2 MB (74.00%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
-    int numSubseq(vector<int>& nums, int target) 
-    {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        long res = 0;
-        int k = 1'000'000'007;
-        vector<long> dp(n + 1, 1);
-        for(int i = 1; i <= n; ++i)
-        {
-            dp[i] = (dp[i - 1] << 1) % k;
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        long long ans=0;
+        int mod=1e9+7;
+        int powers[100001]={};
+        powers[0]=1;
+        for(int i=1;i<100001;i++){
+            powers[i]=(powers[i-1]*2)%mod;
         }
-        for(int i = 0, j = n - 1; i < n; ++i)
+        for(int i=0;i<nums.size();i++)
         {
-            while(j >= i && nums[i] + nums[j] > target) 
+            int maxi=find(nums,i,target);
+            if(maxi!=-1)
             {
-                --j;
+                int y=maxi-i;
+                ans=(ans+powers[y])%mod;
             }
-            if(i > j) 
-            {
-                continue;
-            } 
-            res = (res + dp[j - i]) % k; 
         }
-        return res;
+        return ans%mod;
+        
+    }
+    int find(vector<int>& nums,int ind,int target)
+    {
+        int ans=-1;
+        int l=ind,h=nums.size()-1;
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            if(nums[ind]+nums[mid]<=target)
+            {
+                ans=mid;
+                l=mid+1;
+            }
+            else
+            h=mid-1;
+        }
+        return ans;
     }
 };
