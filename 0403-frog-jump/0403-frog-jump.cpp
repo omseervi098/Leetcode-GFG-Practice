ef104d2a44a94b45c10591d1aef50b1b4bf92a57
Omprakash Choudhary <om78900987@gmail.com>
2023-08-27 16:48:25 +0530

    Time: 59 ms (90.13%), Space: 211.6 MB (28.33%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -10,18 +10,29 @@ public:
         //3 Options k-1,k,k+1
         bool a=false,b=false,c=false;
         cout<<stones[st]<<" "<<k<<endl;
-        if(s.count(stones[st]+k)!=0) 
-            b=helper(k,s[stones[st]+k],end,stones,dp);
-        if(s.count(stones[st]+k+1)!=0)
+        if(s.count(stones[st]+k+1)==0) c=false;
+        else{
             c=helper(k+1,s[stones[st]+k+1],end,stones,dp);
-        if(k>1 and s.count(stones[st]+k-1)!=0)
-            a=helper(k-1,s[stones[st]+k-1],end,stones,dp);
+            if(c) return c;
+        }
+        if(s.count(stones[st]+k)==0) b=false;
+        else {
+            b=helper(k,s[stones[st]+k],end,stones,dp);
+            if(b) return b;
+        }
+         if(k!=1){
+            if(s.count(stones[st]+k-1)==0) a=false;
+            else { 
+                a=helper(k-1,s[stones[st]+k-1],end,stones,dp);
+                if(a) return a;
+            }
+        }
         
         return dp[st][k]=a || b || c;
     }
     bool canCross(vector<int>& stones) {
         int n=stones.size();
-        vector<vector<int>> dp(n,vector<int>(2000,-1));
+        vector<vector<int>> dp(n,vector<int>(1000,-1));
         for(int i=0;i<stones.size();i++)  s[stones[i]]=i;
         int k=1;
         if(s.count(stones[0]+k)==0) return false;
