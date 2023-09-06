d586c62a81f4528643a40fc4374c1655158c4550
Omprakash Choudhary <om78900987@gmail.com>
2023-09-06 07:41:40 +0530

    Time: 2685 ms (5.01%), Space: 582.7 MB (5.02%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+ 
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        set<vector<int>> v;
+        sort(nums.begin(),nums.end());
+        unordered_map<long long,int> m;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                for(int k=j+1;k<n;k++){
+            
+                    long long temp=(long long)target-(long long)nums[k]-(long long)nums[i]-(long long)nums[j];
+                    while(m[temp]>0){
+                        v.insert({nums[i],nums[j],(int)temp,nums[k]});
+                        m[temp]--;
+                    }
+                    m[nums[k]]++;
+                }
+                m.clear();
+            }
+        }
+        vector<vector<int>> ans;
+        for(auto i:v) ans.push_back(i);
+        return ans;
+    }
+};
