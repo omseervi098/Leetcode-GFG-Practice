d86adf87c3e5d08e0a2c1e121a20a1d508b587f4
Omprakash Choudhary <om78900987@gmail.com>
2023-10-01 10:06:30 +0530

    Time: 9 ms (62.09%), Space: 16.4 MB (7.39%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -5,26 +5,26 @@ public:
         while(i<nums.size() and nums[i]==ele1 ) {i++;cnt1++;}
         if(i==nums.size()) return {ele1};
         ele2=nums[i++];cnt2=1;
-        cout<<ele1<<" "<<ele2<<endl;
-        cout<<cnt1<<" "<<cnt2<<endl;
         for(;i<nums.size();i++){
-           
             if(cnt1==0 and nums[i]!=ele2){
                 ele1=nums[i];
+                cnt1++;
+            } 
+            else if(cnt2==0 and nums[i]!=ele1) {
+                ele2=nums[i];
+                cnt2++;
             }
-            if(cnt2==0 and nums[i]!=ele1) ele2=nums[i];
-             if(nums[i]!=ele1 and nums[i]!=ele2) {cnt1--;cnt2--;}
-            if(nums[i]==ele1) cnt1++;
-            if(nums[i]==ele2) cnt2++;
-            
+            else if(nums[i]==ele1) cnt1++;
+            else if(nums[i]==ele2) cnt2++;
+            else{
+                cnt1--;cnt2--;
+            }
         }
-        cout<<ele1<<" "<<ele2<<endl;
         cnt1=0,cnt2=0;
         for(int i=0;i<nums.size();i++){
             if(nums[i]==ele1) cnt1++;
             if(nums[i]==ele2) cnt2++;
         }
-        cout<<cnt1<<" "<<cnt2<<endl;
         vector<int> v;
         if(cnt1>nums.size()/3.0) v.push_back(ele1);
         if(cnt2>nums.size()/3.0) v.push_back(ele2);
