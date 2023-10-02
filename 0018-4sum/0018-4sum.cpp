0abb6abef317732e23df0f82587eb38b1343a633
Omprakash Choudhary <om78900987@gmail.com>
2023-10-02 22:28:15 +0530

    Time: 50 ms (86.85%), Space: 13.9 MB (33.24%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,22 +1,30 @@
 #define ll long long
+#define umap unordered_map<long long,int>
 class Solution {
     public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n=nums.size();
         set<vector<int>> v;
         sort(nums.begin(),nums.end());
-        unordered_map<long long,int> m;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                for(int k=j+1;k<n;k++){
-                    ll temp=(ll)target-(ll)nums[k]-(ll)nums[i]-(ll)nums[j];
-                    while(m[temp]>0){
-                        v.insert({nums[i],nums[j],(int)temp,nums[k]});
-                        m[temp]--;
+        for(int i=0;i<n-2;i++){
+            if(i>0 and nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n-1;j++){
+                if(j>i+1 and nums[j]==nums[j-1]) continue;
+               ll ntarget=(ll)target-(ll)nums[i]-(ll)nums[j];
+                int l=j+1,h=n-1;
+                while(l<h){
+                    if(nums[l]+nums[h]<ntarget){
+                        l++;
+                    }else if(nums[l]+nums[h]>ntarget) h--;
+                    else {
+                        v.insert({nums[i],nums[j],nums[l],nums[h]});
+                        int tempst=l,tempend=h;
+                        while(tempst<tempend and nums[l]==nums[tempst]) tempst++;
+                        while(tempend>tempst and nums[h]==nums[tempend]) tempend--;
+                        l=tempst;
+                        h=tempend;
                     }
-                    m[nums[k]]++;
                 }
-                m.clear();
             }
         }
         vector<vector<int>> ans;
