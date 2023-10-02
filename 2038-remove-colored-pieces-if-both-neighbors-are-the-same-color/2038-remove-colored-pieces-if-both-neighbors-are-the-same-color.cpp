efc819f8632a26528faae397389319c5521df430
Omprakash Choudhary <om78900987@gmail.com>
2023-10-02 21:15:48 +0530

    Time: 36 ms (38.86%), Space: 13.6 MB (17.42%) - LeetHub

--- 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2038-remove-colored-pieces-if-both-neighbors-are-the-same-color/2038-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     bool winnerOfGame(string c) {
-        vector<int> cnta,cntb;
+        int sm1=0,sm2=0;
         for(int i=0;i<c.size();){
             int j=i;
-            while(i<c.size() and c[i]==c[j]){
-                i++;
-            }
-            if(c[j]=='A') cnta.push_back(i-j);
-            else cntb.push_back(i-j);
+            while(i<c.size() and c[i]==c[j]) i++;
+            if(c[j]=='A') sm1+=max(i-j-2,0);
+            else sm2+=max(i-j-2,0);
         }
-        int sm1=0,sm2=0;
-        for(auto i:cnta) sm1+=max(i-2,0);
-        for(auto i:cntb) sm2+=max(i-2,0);
         if(sm2>=sm1) return false;
         return true;
     }
