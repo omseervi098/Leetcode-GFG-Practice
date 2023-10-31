379e9da6f54fc06681490a7c15676c1b71923378
Omprakash Choudhary <om78900987@gmail.com>
2023-10-31 23:19:22 +0530

    Time: 2 ms (51.62%), Space: 6.4 MB (18.91%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int kthGrammar(int n, int k) {
-        if(n==1) return 0;
-        if(n==2) return k==1?0:1;
-        if(n==3) {
+        if(n<=3) {
             string temp="0110";
             return temp[k-1]-'0';
         }
