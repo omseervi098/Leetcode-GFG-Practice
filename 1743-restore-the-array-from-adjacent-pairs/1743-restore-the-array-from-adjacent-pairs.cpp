9d1070ed7444c57e3af0ad52d019c3f41fc0df26
Omprakash Choudhary <om78900987@gmail.com>
2023-11-11 21:28:16 +0530

    Time: 456 ms (46.23%), Space: 129.1 MB (49.55%) - LeetHub

--- 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
+++ 1743-restore-the-array-from-adjacent-pairs/1743-restore-the-array-from-adjacent-pairs.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     vector<int> restoreArray(vector<vector<int>>& ap) {
         unordered_map<int,vector<int>> mp;
+        vector<int> v;
+        set<int> visited;
         for(auto i:ap){
             mp[i[0]].push_back(i[1]);
             mp[i[1]].push_back(i[0]);
         }
-        int temp1=0,n=ap.size()+1;
+        int temp1=0;
         for(auto i:mp){
             if(i.second.size()==1){
                 temp1=i.first;
                 break;
             }
         }
-        vector<int> v;
-        set<int> visited;
         v.push_back(temp1);
         visited.insert(temp1);
         int temp=mp[temp1][0];
