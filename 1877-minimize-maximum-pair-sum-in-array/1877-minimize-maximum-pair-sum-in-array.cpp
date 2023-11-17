8690f55d2e0edad72b81a98ba068be072deba58e
Omprakash Choudhary <om78900987@gmail.com>
2023-11-17 13:56:02 +0530

    Time: 187 ms (57.87%), Space: 96.5 MB (61.58%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.cpp
@@ -2,11 +2,8 @@ class Solution {
 public:
     int minPairSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        int st=0,end=nums.size()-1;
-        int mx=0;
-        while(st<end){
-            mx=max(mx,nums[st++]+nums[end--]);
-        }
+        int st=0,end=nums.size()-1,mx=0;
+        while(st<end) mx=max(mx,nums[st++]+nums[end--]);
         return mx;
     }
 };
