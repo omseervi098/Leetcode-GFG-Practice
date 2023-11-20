763f2653d5cce08c246078a58539162528de5720
Omprakash Choudhary <om78900987@gmail.com>
2023-11-20 22:42:48 +0530

    Time: 401 ms (24.32%), Space: 141.3 MB (19.36%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -5,10 +5,8 @@ public:
         unordered_map<int,int> mp;
         for(int i=0;i<nums.size();i++) mp[nums[i]]=i;
         for(auto i:op){
-            int idx=mp[i[0]];
             nums[mp[i[0]]]=i[1];
-            mp.erase(i[0]);
-            mp[i[1]]=idx;
+            mp[i[1]]=mp[i[0]];
         }
         return nums;
     }
