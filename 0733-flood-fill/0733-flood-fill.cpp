92c9637564606d140ad8ff4f26bcbf333abd28da
Omprakash Choudhary <om78900987@gmail.com>
2023-11-29 17:44:18 +0530

    Time: 10 ms (22.04%), Space: 14.8 MB (13.37%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,21 +3,22 @@ class Solution {
         queue<pair<int,int>>q;
         q.push({sr,sc});
         int oldcolor=i[sr][sc];
+        i[sr][sc]=ncol;
         vis[sr][sc]=1;
         vector<pair<int,int>> dir={{-1,0},{0,1},{1,0},{0,-1}};
         while(q.size()){
             auto front=q.front();q.pop();
-            i[front.first][front.second]=ncol;
+            
             for(auto [dr,dc]:dir){
                     int newr=front.first+dr;
                     int newc=front.second+dc;
                     if(newr>=0 && newc>=0 && newr<n && newc<m && vis[newr][newc]!=1 && i[newr][newc]==oldcolor){
                         q.push({newr,newc});
                         vis[newr][newc]=1;
+                        i[newr][newc]=ncol;
                     }
-                }
             }
-    
+        }
     }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
