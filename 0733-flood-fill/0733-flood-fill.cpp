7cda521d2b786b2a52f98ed1e523271f87586f58
Omprakash Choudhary <om78900987@gmail.com>
2023-11-29 17:43:43 +0530

    Time: 6 ms (69.21%), Space: 15 MB (7.62%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    void bfs(int sr,int sc,int n,int m,int ncol,vector<vector<int>> &i,vector<vector<int>> &vis){
+        queue<pair<int,int>>q;
+        q.push({sr,sc});
+        int oldcolor=i[sr][sc];
+        vis[sr][sc]=1;
+        vector<pair<int,int>> dir={{-1,0},{0,1},{1,0},{0,-1}};
+        while(q.size()){
+            auto front=q.front();q.pop();
+            i[front.first][front.second]=ncol;
+            for(auto [dr,dc]:dir){
+                    int newr=front.first+dr;
+                    int newc=front.second+dc;
+                    if(newr>=0 && newc>=0 && newr<n && newc<m && vis[newr][newc]!=1 && i[newr][newc]==oldcolor){
+                        q.push({newr,newc});
+                        vis[newr][newc]=1;
+                    }
+                }
+            }
+    
+    }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n=image.size(),m=image[0].size();
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        bfs(sr,sc,n,m,color,image,visited);
+        return image;
+    }
+};
