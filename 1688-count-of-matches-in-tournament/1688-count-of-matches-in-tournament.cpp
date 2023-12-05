1972410f950f8cd618fbe289af4095cd55e76283
Omprakash Choudhary <om78900987@gmail.com>
2023-12-05 10:36:23 +0530

    Time: 2 ms (44.05%), Space: 6.2 MB (73.09%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int cnt=0;
+        while(n>1){
+            if(n%2==0){ 
+                cnt+=n/2;
+                n-=n/2;
+            }
+            else{
+                cnt+=(n-1)/2;
+                n-=(n-1)/2;
+            }
+        }
+        return cnt;
+    }
+};
