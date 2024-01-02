77a5d747e39f8021c51d17c8f73bc8839cdc5644
Omprakash Choudhary <om78900987@gmail.com>
2024-01-02 09:04:12 +0530

    Time: 43 ms (5.35%), Space: 30.4 MB (35.48%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        unordered_map<int,int> mp;int mx=0;
-        for(auto i:nums) {mp[i]++;mx=max(mx,mp[i]);}    
+        unordered_map<int,int> mp;
+        int mx=0;
+        for(auto i:nums){ 
+            mp[i]++;
+            mx=max(mx,mp[i]);
+        }
         vector<vector<int>> v;
-        while(mx-->0){
+        for(int i=0;i<mx;i++){
             vector<int> temp;
-            for(auto &j:mp){
-                if(j.second>0){
-                    temp.push_back(j.first);
-                    j.second-=1;
+            for(auto &i:mp){
+                if(i.second>0){
+                    temp.push_back(i.first);
+                    i.second--;
                 }
             }
             v.push_back(temp);
-        }
-        return v;
-        
+        }   return v;
     }
 };
